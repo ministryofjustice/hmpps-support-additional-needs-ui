95a5f54b0127a8d27821cc197b754801e3216ca3
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2025-05-21 10:03:19 +0100

    RR-1471 - Get user caseloads

--- server/services/index.ts
+++ server/services/index.ts
@@ -2,19 +2,28 @@ import { dataAccess } from '../data'
 import AuditService from './auditService'
 import JourneyDataService from './journeyDataService'
 import PrisonService from './prisonService'
+import UserService from './userService'
 
 export const services = () => {
-  const { applicationInfo, hmppsAuditClient, journeyDataStore, prisonRegisterClient, prisonRegisterStore } =
-    dataAccess()
+  const {
+    applicationInfo,
+    hmppsAuditClient,
+    journeyDataStore,
+    prisonRegisterClient,
+    prisonRegisterStore,
+    managedUsersApiClient,
+    userCaseLoadDetailStore,
+  } = dataAccess()
 
   return {
     applicationInfo,
     auditService: new AuditService(hmppsAuditClient),
     journeyDataService: new JourneyDataService(journeyDataStore),
     prisonService: new PrisonService(prisonRegisterStore, prisonRegisterClient),
+    userService: new UserService(managedUsersApiClient, userCaseLoadDetailStore),
   }
 }
 
 export type Services = ReturnType<typeof services>
 
-export { AuditService, JourneyDataService, PrisonService }
+export { AuditService, JourneyDataService, PrisonService, UserService }
