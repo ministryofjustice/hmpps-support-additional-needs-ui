81dc485f58319a6a2817c2edfe75edaa47319d7c
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2025-05-28 18:58:46 +0100

    RR-1514 - Add layout and empty state content to Overview page

--- server/routes/profile/overview/overviewController.ts
+++ server/routes/profile/overview/overviewController.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, RequestHandler, Response } from 'express'
 export default class OverviewController {
   getOverviewView: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
     const { prisonerSummary } = res.locals
-    const viewRenderArgs = { prisonerSummary }
+    const viewRenderArgs = { prisonerSummary, tab: 'overview' }
     return res.render('pages/profile/overview/index', viewRenderArgs)
   }
 }
