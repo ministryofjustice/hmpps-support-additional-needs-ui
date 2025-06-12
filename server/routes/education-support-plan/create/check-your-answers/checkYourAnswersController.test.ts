e311e78def36962808c2310c8f0a472fd56ecfd7
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2025-06-12 16:29:10 +0100

    RR-1560 - Call API to save the Education Support Plan (#71)

--- server/routes/education-support-plan/create/check-your-answers/checkYourAnswersController.test.ts
+++ server/routes/education-support-plan/create/check-your-answers/checkYourAnswersController.test.ts
@@ -2,18 +2,27 @@ import { Request, Response } from 'express'
 import aValidPrisonerSummary from '../../../../testsupport/prisonerSummaryTestDataBuilder'
 import aValidEducationSupportPlanDto from '../../../../testsupport/educationSupportPlanDtoTestDataBuilder'
 import CheckYourAnswersController from './checkYourAnswersController'
+import EducationSupportPlanService from '../../../../services/educationSupportPlanService'
+
+jest.mock('../../../../services/educationSupportPlanService')
 
 describe('checkYourAnswersController', () => {
-  const controller = new CheckYourAnswersController()
+  const educationSupportPlanService = new EducationSupportPlanService(null) as jest.Mocked<EducationSupportPlanService>
+  const controller = new CheckYourAnswersController(educationSupportPlanService)
 
+  const username = 'A_USER'
   const prisonNumber = 'A1234BC'
   const prisonerSummary = aValidPrisonerSummary({ prisonNumber })
   const educationSupportPlanDto = aValidEducationSupportPlanDto({ prisonNumber })
+
+  const flash = jest.fn()
 
   const req = {
     session: {},
     journeyData: {},
     body: {},
+    user: { username },
+    flash,
   } as unknown as Request
   const res = {
     redirect: jest.fn(),
@@ -32,12 +41,15 @@ describe('checkYourAnswersController', () => {
     }
   })
 
-  it('should render view', async () => {
+  it('should render view given no api errors from previous submission', async () => {
     // Given
+    flash.mockReturnValue([])
+
     const expectedViewTemplate = 'pages/education-support-plan/check-your-answers/index'
     const expectedViewModel = {
       prisonerSummary,
       dto: aValidEducationSupportPlanDto(),
+      errorSavingEducationSupportPlan: false,
     }
 
     // When
@@ -47,8 +59,30 @@ describe('checkYourAnswersController', () => {
     expect(res.render).toHaveBeenCalledWith(expectedViewTemplate, expectedViewModel)
   })
 
-  it('should submit form and redirect to next route', async () => {
+  it('should render view given api errors from previous submission', async () => {
     // Given
+    flash.mockReturnValue(['true'])
+
+    const expectedViewTemplate = 'pages/education-support-plan/check-your-answers/index'
+    const expectedViewModel = {
+      prisonerSummary,
+      dto: aValidEducationSupportPlanDto(),
+      errorSavingEducationSupportPlan: true,
+    }
+
+    // When
+    await controller.getCheckYourAnswersView(req, res, next)
+
+    // Then
+    expect(res.render).toHaveBeenCalledWith(expectedViewTemplate, expectedViewModel)
+  })
+
+  it('should submit form and redirect to next route given calling API is successful', async () => {
+    // Given
+    educationSupportPlanService.createEducationSupportPlan.mockResolvedValue(
+      aValidEducationSupportPlanDto({ prisonNumber }),
+    )
+
     const expectedNextRoute = `/profile/${prisonNumber}/overview`
 
     // When
@@ -57,5 +91,29 @@ describe('checkYourAnswersController', () => {
     // Then
     expect(res.redirectWithSuccess).toHaveBeenCalledWith(expectedNextRoute, 'Education support plan created')
     expect(req.journeyData.educationSupportPlanDto).toBeUndefined()
+    expect(flash).not.toHaveBeenCalled()
+    expect(educationSupportPlanService.createEducationSupportPlan).toHaveBeenCalledWith(
+      username,
+      educationSupportPlanDto,
+    )
+  })
+
+  it('should submit form and redirect to next route given calling API is not successful', async () => {
+    // Given
+    educationSupportPlanService.createEducationSupportPlan.mockRejectedValue(new Error('Internal Server Error'))
+
+    const expectedNextRoute = 'check-your-answers'
+
+    // When
+    await controller.submitCheckYourAnswersForm(req, res, next)
+
+    // Then
+    expect(res.redirect).toHaveBeenCalledWith(expectedNextRoute)
+    expect(req.journeyData.educationSupportPlanDto).toEqual(educationSupportPlanDto)
+    expect(flash).toHaveBeenCalledWith('pageHasApiErrors', 'true')
+    expect(educationSupportPlanService.createEducationSupportPlan).toHaveBeenCalledWith(
+      username,
+      educationSupportPlanDto,
+    )
   })
 })
