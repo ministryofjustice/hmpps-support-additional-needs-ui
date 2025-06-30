bd50acba6c2026d049693ce3657c55eb0632e2c9
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2025-06-30 14:55:19 +0100

    RR-1625 - Initial route for refuse-plan journey (#90)

---
+++ server/routes/education-support-plan/refuse-plan/index.ts
@@ -0,0 +1,34 @@
+import { Router } from 'express'
+import { Services } from '../../../services'
+import insertJourneyIdentifier from '../../../middleware/insertJourneyIdentifier'
+import setupJourneyData from '../../../middleware/setupJourneyData'
+import asyncMiddleware from '../../../middleware/asyncMiddleware'
+import ReasonController from './reason/reasonController'
+
+const refuseEducationSupportPlanRoutes = (services: Services): Router => {
+  const { journeyDataService } = services
+  const router = Router({ mergeParams: true })
+
+  const reasonController = new ReasonController()
+
+  router.use('/', [
+    // TODO - enable this line when we understand the RBAC roles and permissions
+    // checkUserHasPermissionTo(ApplicationAction.EXEMPT_EDUCATION_LEARNER_SUPPORT_PLAN),
+    insertJourneyIdentifier({ insertIdAfterElement: 3 }), // insert journey ID immediately after '/education-support-plan/:prisonNumber/refuse-plan' - eg: '/education-support-plan/A1234BC/refuse-plan/473e9ee4-37d6-4afb-92a2-5729b10cc60f/reason'
+  ])
+  router.use('/:journeyId', [setupJourneyData(journeyDataService)])
+
+  router.get('/:journeyId/reason', [
+    // createEmptyEducationSupportPlanDtoIfNotInJourneyData,
+    asyncMiddleware(reasonController.getRefusePlanReasonView),
+  ])
+  router.post('/:journeyId/reason', [
+    // checkEducationSupportPlanDtoExistsInJourneyData,
+    // validate(whoCompletedThePlanSchema),
+    asyncMiddleware(reasonController.submitRefusePlanReasonView),
+  ])
+
+  return router
+}
+
+export default refuseEducationSupportPlanRoutes
