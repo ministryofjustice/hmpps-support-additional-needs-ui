2046b23fd0e6226997cdc86f86662360988080e2
srowlands-moj <145464259+srowlands-moj@users.noreply.github.com>
2025-07-15 11:54:41 +0100

    RR-1679 add challenges and extend actions menu macro (#125)
    * Adds challenge entry page with initial implementation of extendable action menu

--- server/views/components/actions-card/actionsCard.test.ts
+++ server/views/components/actions-card/actionsCard.test.ts
@@ -40,4 +40,37 @@ describe('Tests for actions card component', () => {
     // Then
     expect($('[data-qa=education-support-plan-actions]').length).toEqual(1)
   })
+
+  it('should render the actions card component based on menu type', () => {
+    // Given
+    const params = {
+      ...templateParams,
+      actionMenuType: 'challenges',
+    }
+
+    // When
+    const content = nunjucks.render('actionCardChallenges.test.njk', params)
+    const $ = cheerio.load(content)
+
+    // Then
+    expect($('[data-qa=challenges-actions]').length).toEqual(1)
+    expect($('[data-qa=add-challenge-button]').length).toEqual(1)
+    expect($('[data-qa=record-screener-results-button]').length).toEqual(1)
+  })
+
+  it('should render the actions card component based on menu type, default to esp actions', () => {
+    // Given
+    const params = {
+      ...templateParams,
+      actionMenuType: '',
+    }
+
+    // When
+    const content = nunjucks.render('actionCards.test.njk', params)
+    const $ = cheerio.load(content)
+
+    // Then
+    // When no option is provided, default to the education support plan menu
+    expect($('[data-qa=education-support-plan-actions]').length).toEqual(1)
+  })
 })
