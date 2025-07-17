0196782d3f55cec0ea91ae3c5c1f3df206b30e3b
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2025-07-17 09:46:02 +0100

    RR-1684 - Zod validator for the "detail" page; submit form & call API (#132)

--- server/routes/strengths/create/detail/detailController.ts
+++ server/routes/strengths/create/detail/detailController.ts
@@ -1,15 +1,50 @@
 import { NextFunction, Request, Response } from 'express'
 import type { StrengthDto } from 'dto'
+import StrengthIdentificationSource from '../../../../enums/strengthIdentificationSource'
+import { StrengthService } from '../../../../services'
+import { Result } from '../../../../utils/result/result'
+import { asArray } from '../../../../utils/utils'
 
 export default class DetailController {
+  constructor(private readonly strengthService: StrengthService) {}
+
   getDetailView = async (req: Request, res: Response, next: NextFunction) => {
     const { invalidForm } = res.locals
     const { strengthDto } = req.journeyData
 
-    const detailForm = invalidForm ?? this.populateFormFromDto(strengthDto)
+    const detailForm = invalidForm
+      ? {
+          ...invalidForm,
+          howIdentified: asArray(invalidForm.howIdentified),
+        }
+      : this.populateFormFromDto(strengthDto)
 
-    const viewRenderArgs = { form: detailForm, category: strengthDto.strengthTypeCode }
+    const viewRenderArgs = {
+      form: detailForm,
+      category: strengthDto.strengthTypeCode,
+      errorRecordingStrength: req.flash('pageHasApiErrors')[0] != null,
+    }
     return res.render('pages/strengths/detail/index', viewRenderArgs)
+  }
+
+  submitDetailForm = async (req: Request, res: Response, next: NextFunction) => {
+    const detailForm = { ...req.body }
+    this.updateDtoFromForm(req, detailForm)
+
+    const { strengthDto } = req.journeyData
+    const { apiErrorCallback } = res.locals
+    const apiResult = await Result.wrap(
+      this.strengthService.createStrengths(req.user.username, [strengthDto]),
+      apiErrorCallback,
+    )
+    if (!apiResult.isFulfilled()) {
+      req.flash('pageHasApiErrors', 'true')
+      return res.redirect('detail')
+    }
+
+    const { prisonNumber } = strengthDto
+    req.journeyData.strengthDto = undefined
+    return res.redirectWithSuccess(`/profile/${prisonNumber}/strengths`, 'Strength added')
   }
 
   private populateFormFromDto = (dto: StrengthDto) => {
@@ -18,5 +53,16 @@ export default class DetailController {
       howIdentified: dto.howIdentified || [],
       howIdentifiedOther: dto.howIdentifiedOther,
     }
+  }
+
+  private updateDtoFromForm = (
+    req: Request,
+    form: { description: string; howIdentified: Array<StrengthIdentificationSource>; howIdentifiedOther?: string },
+  ) => {
+    const { strengthDto } = req.journeyData
+    strengthDto.symptoms = form.description
+    strengthDto.howIdentified = form.howIdentified
+    strengthDto.howIdentifiedOther = form.howIdentifiedOther
+    req.journeyData.strengthDto = strengthDto
   }
 }
