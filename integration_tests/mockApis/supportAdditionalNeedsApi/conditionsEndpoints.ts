6f698752064d05a2167aba1331199ca8a17f179d
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2025-08-04 11:00:54 +0100

    RR-1711 - Call middleware to retrieve prisoner conditions; initial skeleton view (#196)

--- integration_tests/mockApis/supportAdditionalNeedsApi/conditionsEndpoints.ts
+++ integration_tests/mockApis/supportAdditionalNeedsApi/conditionsEndpoints.ts
@@ -1,4 +1,5 @@
 import { SuperAgentRequest } from 'superagent'
+import type { ConditionResponse } from 'supportAdditionalNeedsApiClient'
 import { stubFor } from '../wiremock'
 
 const stubCreateConditions = (prisonNumber = 'G6115VJ'): SuperAgentRequest =>
@@ -61,4 +62,66 @@ const stubCreateConditions500Error = (prisonNumber = 'G6115VJ'): SuperAgentReque
     },
   })
 
-export default { stubCreateConditions, stubCreateConditions500Error }
+const stubGetConditions = (
+  options: { prisonNumber: string; conditions?: Array<ConditionResponse> } = { prisonNumber: 'G6115VJ' },
+): SuperAgentRequest =>
+  stubFor({
+    request: {
+      method: 'GET',
+      urlPattern: `/support-additional-needs-api/profile/${options.prisonNumber}/conditions`,
+    },
+    response: {
+      status: 201,
+      headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+      jsonBody: {
+        conditions: options.conditions ?? [
+          {
+            createdBy: 'asmith_gen',
+            createdByDisplayName: 'Alex Smith',
+            createdAt: '2023-06-19T09:39:44Z',
+            createdAtPrison: 'BXI',
+            updatedBy: 'asmith_gen',
+            updatedByDisplayName: 'Alex Smith',
+            updatedAt: '2023-06-19T09:39:44Z',
+            updatedAtPrison: 'BXI',
+            reference: 'c88a6c48-97e2-4c04-93b5-98619966447b',
+            source: 'SELF_DECLARED',
+            conditionType: {
+              code: 'string',
+              description: 'string',
+              categoryCode: 'string',
+              categoryDescription: 'string',
+              areaCode: 'string',
+              areaDescription: 'string',
+              listSequence: 3,
+              active: true,
+            },
+            conditionName: '(for Mental Health) Social Anxiety',
+            conditionDetails: 'string',
+            active: true,
+          },
+        ],
+      },
+    },
+  })
+
+const stubGetConditions500Error = (prisonNumber = 'G6115VJ'): SuperAgentRequest =>
+  stubFor({
+    request: {
+      method: 'GET',
+      urlPattern: `/support-additional-needs-api/profile/${prisonNumber}/conditions`,
+    },
+    response: {
+      status: 500,
+      headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+      jsonBody: {
+        status: 500,
+        errorCode: null,
+        userMessage: 'An unexpected error occurred',
+        developerMessage: 'An unexpected error occurred',
+        moreInfo: null,
+      },
+    },
+  })
+
+export default { stubCreateConditions, stubCreateConditions500Error, stubGetConditions, stubGetConditions500Error }
--- server/routes/profile/conditions/conditionsController.test.ts
+++ server/routes/profile/conditions/conditionsController.test.ts
@@ -1,16 +1,19 @@
 import { Request, Response } from 'express'
 import ConditionsController from './conditionsController'
 import aValidPrisonerSummary from '../../../testsupport/prisonerSummaryTestDataBuilder'
+import { aValidConditionsList } from '../../../testsupport/conditionDtoTestDataBuilder'
+import { Result } from '../../../utils/result/result'
 
 describe('conditionsController', () => {
   const controller = new ConditionsController()
 
   const prisonerSummary = aValidPrisonerSummary()
+  const conditions = Result.fulfilled(aValidConditionsList())
 
   const req = {} as unknown as Request
   const res = {
     render: jest.fn(),
-    locals: { prisonerSummary },
+    locals: { prisonerSummary, conditions },
   } as unknown as Response
   const next = jest.fn()
 
@@ -23,6 +26,7 @@ describe('conditionsController', () => {
     const expectedViewTemplate = 'pages/profile/conditions/index'
     const expectedViewModel = {
       prisonerSummary,
+      conditions,
       tab: 'conditions',
     }
 
