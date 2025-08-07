d8ae421a60de0b56c77136e5c868704bc33afcdf
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2025-08-07 10:00:30 +0100

    RR-1669 - Fix mapping of "NONE" in  create-screener API request object (#215)

--- server/data/mappers/alnScreenerRequestMapper.test.ts
+++ server/data/mappers/alnScreenerRequestMapper.test.ts
@@ -28,4 +28,50 @@ describe('alnScreenerRequestMapper', () => {
     // Then
     expect(actual).toEqual(expected)
   })
+
+  it('should map an AlnScreenerDto to an AlnScreenerRequest given challenges is NONE', () => {
+    // Given
+    const alnScreenerDto = aValidAlnScreenerDto({
+      prisonId: 'MDI',
+      screenerDate: startOfToday(),
+      challenges: [ChallengeType.NONE],
+      strengths: [StrengthType.READING, StrengthType.LISTENING],
+    })
+
+    const expected = aValidAlnScreenerRequest({
+      prisonId: 'MDI',
+      screenerDate: startOfToday(),
+      challenges: [], // expect NONE to be mapped as an empty array
+      strengths: [{ strengthTypeCode: StrengthType.READING }, { strengthTypeCode: StrengthType.LISTENING }],
+    })
+
+    // When
+    const actual = toAlnScreenerRequest(alnScreenerDto)
+
+    // Then
+    expect(actual).toEqual(expected)
+  })
+
+  it('should map an AlnScreenerDto to an AlnScreenerRequest given strengths is NONE', () => {
+    // Given
+    const alnScreenerDto = aValidAlnScreenerDto({
+      prisonId: 'MDI',
+      screenerDate: startOfToday(),
+      challenges: [ChallengeType.ARITHMETIC, ChallengeType.NUMBER_RECALL],
+      strengths: [StrengthType.NONE],
+    })
+
+    const expected = aValidAlnScreenerRequest({
+      prisonId: 'MDI',
+      screenerDate: startOfToday(),
+      challenges: [{ challengeTypeCode: ChallengeType.ARITHMETIC }, { challengeTypeCode: ChallengeType.NUMBER_RECALL }],
+      strengths: [], // expect NONE to be mapped as an empty array
+    })
+
+    // When
+    const actual = toAlnScreenerRequest(alnScreenerDto)
+
+    // Then
+    expect(actual).toEqual(expected)
+  })
 })
--- server/data/mappers/alnScreenerRequestMapper.ts
+++ server/data/mappers/alnScreenerRequestMapper.ts
@@ -1,12 +1,18 @@
 import { format } from 'date-fns'
 import type { AlnScreenerRequest } from 'supportAdditionalNeedsApiClient'
 import type { AlnScreenerDto } from 'dto'
+import ChallengeType from '../../enums/challengeType'
+import StrengthType from '../../enums/strengthType'
 
 const toAlnScreenerRequest = (alnScreenerDto: AlnScreenerDto): AlnScreenerRequest => ({
   prisonId: alnScreenerDto.prisonId,
   screenerDate: format(alnScreenerDto.screenerDate, 'yyyy-MM-dd'),
-  challenges: alnScreenerDto.challenges.map(challengeTypeCode => ({ challengeTypeCode })),
-  strengths: alnScreenerDto.strengths.map(strengthTypeCode => ({ strengthTypeCode })),
+  challenges: alnScreenerDto.challenges
+    .filter(challengeTypeCode => challengeTypeCode !== ChallengeType.NONE) // NONE is used to populate the UI; it is not a value that should be mapped to the API request
+    .map(challengeTypeCode => ({ challengeTypeCode })),
+  strengths: alnScreenerDto.strengths
+    .filter(strengthTypeCode => strengthTypeCode !== StrengthType.NONE) // NONE is used to populate the UI; it is not a value that should be mapped to the API request
+    .map(strengthTypeCode => ({ strengthTypeCode })),
 })
 
 export default toAlnScreenerRequest
