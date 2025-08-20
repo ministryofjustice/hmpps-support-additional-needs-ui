a1bef3db30afc906d370a7e25adffaf9408e6774
srowlands-moj <sean.rowlands@digital.justice.gov.uk>
2025-08-20 18:20:24 +0100

    RR-1751-add-cypress-test

--- integration_tests/pages/profile/challengesPage.ts
+++ integration_tests/pages/profile/challengesPage.ts
@@ -2,6 +2,8 @@ import ProfilePage from './profilePage'
 import Page, { PageElement } from '../page'
 import SelectChallengeCategoryPage from '../challenges/selectChallengeCategoryPage'
 import ScreenerDatePage from '../additional-learning-needs-screener/screenerDatePage'
+import ChallengeCategory from '../../../server/enums/challengeCategory'
+import ChallengeType from '../../../server/enums/challengeType'
 
 export default class ChallengesPage extends ProfilePage {
   constructor() {
@@ -25,4 +27,57 @@ export default class ChallengesPage extends ProfilePage {
 
   private recordAlnScreenerButton = (): PageElement =>
     this.challengesActionItems().find('[data-qa=record-screener-results-button]')
+
+  hasChallengesSummaryCard(category: ChallengeCategory): ChallengesPage {
+    this.challengeCategorySummaryCard(category).should('be.visible')
+    return this
+  }
+
+  hasNoChallengesSummaryCard(category: ChallengeCategory): ChallengesPage {
+    this.challengeCategorySummaryCard(category).should('not.exist')
+    return this
+  }
+
+  hasNonAlnChallenges(category: ChallengeCategory, ...challengeType: Array<ChallengeType>): ChallengesPage {
+    challengeType.forEach(challenge =>
+      this.challengeCategorySummaryCard(category)
+        .find(`[data-qa=challenge-summary-list-row-non-aln-${challenge}]`)
+        .should('be.visible'),
+    )
+    return this
+  }
+
+  hasNoNonAlnChallenges(category: ChallengeCategory): ChallengesPage {
+    this.challengeCategorySummaryCard(category)
+      .find(`[data-qa*=challenge-summary-list-row-non-aln-]`)
+      .should('not.exist')
+    return this
+  }
+
+  hasAlnChallenges(category: ChallengeCategory, ...challenges: Array<string>): ChallengesPage {
+    this.challengeCategorySummaryCard(category)
+      .find(`[data-qa=aln-challenge-summary-list-row] li`)
+      .then(listItems => {
+        cy.wrap(listItems).should('have.length', challenges.length)
+        listItems.each((index, element) => {
+          cy.wrap(element).should('contain.text', challenges[index])
+        })
+      })
+    return this
+  }
+
+  hasNoAlnChallenges(category: ChallengeCategory): ChallengesPage {
+    this.challengeCategorySummaryCard(category).find('.aln-challenges').should('not.exist')
+    return this
+  }
+
+  hasNoActiveChallenges(): ChallengesPage {
+    this.noChallengesSummaryCard().should('be.visible')
+    return this
+  }
+
+  private noChallengesSummaryCard = (): PageElement => cy.get('[data-qa=no-challenges-summary-card]')
+
+  private challengeCategorySummaryCard = (category: ChallengeCategory): PageElement =>
+    cy.get(`[data-qa=challenges-summary-card-${category}]`)
 }
