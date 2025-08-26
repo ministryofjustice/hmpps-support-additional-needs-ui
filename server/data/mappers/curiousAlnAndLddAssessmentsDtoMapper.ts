5f24285c3961cf319d2b594d9b2542564bb2a8a5
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2025-08-26 15:06:54 +0100

    RR-1753 - Mapper to map from Curious assessments to our DTOs (#263)

---
+++ server/data/mappers/curiousAlnAndLddAssessmentsDtoMapper.ts
@@ -0,0 +1,51 @@
+import { parseISO } from 'date-fns'
+import type {
+  AllAssessmentDTO,
+  LearnerAssessmentsAlnDTO,
+  LearnerLatestAssessmentV1DTO,
+  LearnerLddInfoExternalV1DTO,
+} from 'curiousApiClient'
+import type { CuriousAlnAndLddAssessmentsDto, CuriousAlnAssessmentDto, CuriousLddAssessmentDto } from 'dto'
+import AlnAssessmentReferral from '../../enums/alnAssessmentReferral'
+
+const toCuriousAlnAndLddAssessmentsDto = (apiResponse: AllAssessmentDTO): CuriousAlnAndLddAssessmentsDto => ({
+  lddAssessments: apiResponse.v1?.flatMap(toCuriousLddAssessmentDtos) || [],
+  alnAssessments: apiResponse.v2?.assessments?.aln?.map(toCuriousAlnAssessmentDto) || [],
+})
+
+const toCuriousLddAssessmentDtos = (apiResponse: LearnerLatestAssessmentV1DTO): Array<CuriousLddAssessmentDto> => {
+  const curiousV1LddAssessments: Array<LearnerLddInfoExternalV1DTO> = apiResponse.ldd
+  return curiousV1LddAssessments
+    .filter(
+      lddAssessment =>
+        lddAssessment.lddPrimaryName && (lddAssessment.rapidAssessmentDate || lddAssessment.inDepthAssessmentDate),
+    )
+    .map(lddAssessment => ({
+      prisonId: lddAssessment.establishmentId,
+      rapidAssessmentDate: lddAssessment.rapidAssessmentDate ? parseISO(lddAssessment.rapidAssessmentDate) : null,
+      inDepthAssessmentDate: lddAssessment.inDepthAssessmentDate ? parseISO(lddAssessment.inDepthAssessmentDate) : null,
+      primaryLddAndHealthNeed: lddAssessment.lddPrimaryName,
+      additionalLddAndHealthNeeds: lddAssessment.lddSecondaryNames || [],
+    }))
+}
+
+const toCuriousAlnAssessmentDto = (curiousV2AlnAssessment: LearnerAssessmentsAlnDTO): CuriousAlnAssessmentDto => ({
+  prisonId: curiousV2AlnAssessment.establishmentId,
+  assessmentDate: parseISO(curiousV2AlnAssessment.assessmentDate),
+  referral: toAlnAssessmentReferral(curiousV2AlnAssessment.stakeholderReferral),
+  supportPlanRequired: curiousV2AlnAssessment.assessmentOutcome?.toLowerCase().trim() === 'yes',
+  hasPrisonerConsent: curiousV2AlnAssessment.hasPrisonerConsent?.toLowerCase().trim() === 'yes',
+})
+
+const toAlnAssessmentReferral = (apiStakeholderReferral: string): AlnAssessmentReferral =>
+  ({
+    healthcare: AlnAssessmentReferral.HEALTHCARE,
+    psychology: AlnAssessmentReferral.PSYCHOLOGY,
+    'education specialist': AlnAssessmentReferral.EDUCATION_SPECIALIST,
+    nsm: AlnAssessmentReferral.NSM,
+    'substance misuse team': AlnAssessmentReferral.SUBSTANCE_MISUSE_TEAM,
+    'safer custody': AlnAssessmentReferral.SAFER_CUSTODY,
+    other: AlnAssessmentReferral.OTHER,
+  })[apiStakeholderReferral.toLowerCase().trim()]
+
+export default toCuriousAlnAndLddAssessmentsDto
--- server/testsupport/curiousAlnAndLddAssessmentsDtoTestDataBuilder.ts
+++ server/testsupport/curiousAlnAndLddAssessmentsDtoTestDataBuilder.ts
@@ -2,65 +2,46 @@ import { startOfDay } from 'date-fns'
 import type { CuriousAlnAndLddAssessmentsDto, CuriousAlnAssessmentDto, CuriousLddAssessmentDto } from 'dto'
 import AlnAssessmentReferral from '../enums/alnAssessmentReferral'
 
-const aCuriousAlnAndLddAssessmentsDto = (
-  options: {
-    lddAssessments?: Array<CuriousLddAssessmentDto>
-    alnAssessments?: Array<CuriousAlnAssessmentDto>
-  } = {
-    lddAssessments: [aCuriousLddAssessmentDto()],
-    alnAssessments: [aCuriousAlnAssessmentDto()],
-  },
-): CuriousAlnAndLddAssessmentsDto => ({
-  lddAssessments: options.lddAssessments,
-  alnAssessments: options.alnAssessments,
+const aCuriousAlnAndLddAssessmentsDto = (options?: {
+  lddAssessments?: Array<CuriousLddAssessmentDto>
+  alnAssessments?: Array<CuriousAlnAssessmentDto>
+}): CuriousAlnAndLddAssessmentsDto => ({
+  lddAssessments: options?.lddAssessments || [aCuriousLddAssessmentDto()],
+  alnAssessments: options?.alnAssessments || [aCuriousAlnAssessmentDto()],
 })
 
-const aCuriousLddAssessmentDto = (
-  options: {
-    prisonId?: string
-    rapidAssessmentDate?: Date
-    inDepthAssessmentDate?: Date
-    primaryLddAndHealthNeed?: string
-    additionalLddAndHealthNeeds?: Array<string>
-  } = {
-    prisonId: 'BXI',
-    rapidAssessmentDate: startOfDay('2024-10-20'),
-    inDepthAssessmentDate: startOfDay('2024-10-25'),
-    primaryLddAndHealthNeed: 'Visual impairment',
-    additionalLddAndHealthNeeds: [
-      'Hearing impairment',
-      'Mental health difficulty',
-      'Social and emotional difficulties',
-    ],
-  },
-): CuriousLddAssessmentDto => ({
-  prisonId: options.prisonId,
-  rapidAssessmentDate: options.rapidAssessmentDate,
-  inDepthAssessmentDate: options.inDepthAssessmentDate,
-  primaryLddAndHealthNeed: options.primaryLddAndHealthNeed,
-  additionalLddAndHealthNeeds: options.additionalLddAndHealthNeeds,
+const aCuriousLddAssessmentDto = (options?: {
+  prisonId?: string
+  rapidAssessmentDate?: Date
+  inDepthAssessmentDate?: Date
+  primaryLddAndHealthNeed?: string
+  additionalLddAndHealthNeeds?: Array<string>
+}): CuriousLddAssessmentDto => ({
+  prisonId: options?.prisonId || 'BXI',
+  rapidAssessmentDate:
+    options?.rapidAssessmentDate === null ? null : options?.rapidAssessmentDate || startOfDay('2024-10-20'),
+  inDepthAssessmentDate:
+    options?.inDepthAssessmentDate === null ? null : options?.inDepthAssessmentDate || startOfDay('2024-10-25'),
+  primaryLddAndHealthNeed: options?.primaryLddAndHealthNeed || 'Visual impairment',
+  additionalLddAndHealthNeeds: options?.additionalLddAndHealthNeeds || [
+    'Hearing impairment',
+    'Mental health difficulty',
+    'Social and emotional difficulties',
+  ],
 })
 
-const aCuriousAlnAssessmentDto = (
-  options: {
-    prisonId?: string
-    assessmentDate?: Date
-    referral?: AlnAssessmentReferral
-    supportPlanRequired?: boolean
-    hasPrisonerConsent?: boolean
-  } = {
-    prisonId: 'BXI',
-    assessmentDate: startOfDay('2025-10-02'),
-    referral: AlnAssessmentReferral.PSYCHOLOGY,
-    supportPlanRequired: true,
-    hasPrisonerConsent: true,
-  },
-): CuriousAlnAssessmentDto => ({
-  prisonId: options.prisonId,
-  assessmentDate: options.assessmentDate,
-  referral: options.referral,
-  supportPlanRequired: options.supportPlanRequired,
-  hasPrisonerConsent: options.hasPrisonerConsent,
+const aCuriousAlnAssessmentDto = (options?: {
+  prisonId?: string
+  assessmentDate?: Date
+  referral?: AlnAssessmentReferral
+  supportPlanRequired?: boolean
+  hasPrisonerConsent?: boolean
+}): CuriousAlnAssessmentDto => ({
+  prisonId: options?.prisonId || 'BXI',
+  assessmentDate: options?.assessmentDate || startOfDay('2025-10-02'),
+  referral: options?.referral || AlnAssessmentReferral.PSYCHOLOGY,
+  supportPlanRequired: options?.supportPlanRequired === false ? false : options?.supportPlanRequired || true,
+  hasPrisonerConsent: options?.hasPrisonerConsent === false ? false : options?.hasPrisonerConsent || true,
 })
 
 export { aCuriousAlnAndLddAssessmentsDto, aCuriousLddAssessmentDto, aCuriousAlnAssessmentDto }
