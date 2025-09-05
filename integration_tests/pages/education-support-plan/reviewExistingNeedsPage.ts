def9d4ccd5c51eac23436157d13847c8166da57b
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2025-09-05 11:53:46 +0100

    RR-1791 - Review Strengths page in Create ELSP journey (#301)

--- integration_tests/pages/education-support-plan/reviewExistingNeedsPage.ts
+++ integration_tests/pages/education-support-plan/reviewExistingNeedsPage.ts
@@ -2,6 +2,6 @@ import Page from '../page'
 
 export default class ReviewExistingNeedsPage extends Page {
   constructor() {
-    super('education-support-plan-review-existng-needs')
+    super('education-support-plan-review-existing-needs')
   }
 }
