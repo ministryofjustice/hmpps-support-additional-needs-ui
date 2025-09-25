fd409bd8b88ae2720e5c5a2ab5bf1942b36a2e6d
srowlands-moj <sean.rowlands@digital.justice.gov.uk>
2025-09-25 13:50:09 +0100

    RR-1913 Update error message

--- server/routes/education-support-plan/validationSchemas/individualSupportRequirementsSchema.ts
+++ server/routes/education-support-plan/validationSchemas/individualSupportRequirementsSchema.ts
@@ -4,8 +4,7 @@ import { createSchema } from '../../../middleware/validationMiddleware'
 const individualSupportRequirementsSchema = async () => {
   const MAX_SUPPORT_REQUIREMENTS_LENGTH = 4000
 
-  const supportRequirementsMandatoryMessage =
-    'Enter details of any support requirements that the individual feels they need'
+  const supportRequirementsMandatoryMessage = 'Add details of the support in education the person feels they need'
   const supportRequirementsMaxLengthMessage = `Any support requirements that the individual feels they need must be ${MAX_SUPPORT_REQUIREMENTS_LENGTH} characters or less`
 
   return createSchema({
