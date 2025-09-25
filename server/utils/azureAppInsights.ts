cf0da84d2fdbfa9d07304a840ad373579a3146bd
srowlands-moj <sean.rowlands@digital.justice.gov.uk>
2025-09-25 09:34:29 +0100

    RR-1890 Add username details to insights data

--- server/utils/azureAppInsights.ts
+++ server/utils/azureAppInsights.ts
@@ -1,4 +1,5 @@
 import {
+  Contracts,
   defaultClient,
   DistributedTracingModes,
   getCorrelationContext,
@@ -6,7 +7,13 @@ import {
   type TelemetryClient,
 } from 'applicationinsights'
 import { RequestHandler } from 'express'
+import { EnvelopeTelemetry } from 'applicationinsights/out/Declarations/Contracts'
 import type { ApplicationInfo } from '../applicationInfo'
+
+export type ContextObject = {
+  /* eslint-disable  @typescript-eslint/no-explicit-any */
+  [name: string]: any
+}
 
 export function initialiseAppInsights(): void {
   if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
@@ -35,6 +42,7 @@ export function buildAppInsightsClient(
       }
       return true
     })
+    defaultClient.addTelemetryProcessor(addUserDataToRequests)
 
     return defaultClient
   }
@@ -53,4 +61,20 @@ export function appInsightsMiddleware(): RequestHandler {
     })
     next()
   }
+}
+
+export function addUserDataToRequests(envelope: EnvelopeTelemetry, contextObjects: ContextObject | undefined): boolean {
+  const isRequest = envelope.data.baseType === Contracts.TelemetryTypeString.Request
+  if (isRequest) {
+    const { username } = contextObjects?.['http.ServerRequest']?.res?.locals?.user || {}
+    if (username) {
+      const { properties } = envelope.data.baseData
+      // eslint-disable-next-line no-param-reassign
+      envelope.data.baseData.properties = {
+        username,
+        ...properties,
+      }
+    }
+  }
+  return true
 }
