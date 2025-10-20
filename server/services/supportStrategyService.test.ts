6197334222355f2ec761fa35b83c9267847e92af
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2025-10-20 16:09:47 +0100

    RR-1856 - Improve Support Strategy mapper and tests (#403)

--- server/services/supportStrategyService.test.ts
+++ server/services/supportStrategyService.test.ts
@@ -1,9 +1,16 @@
+import { parseISO } from 'date-fns'
+import type { SupportStrategyResponseDto } from 'dto'
 import SupportAdditionalNeedsApiClient from '../data/supportAdditionalNeedsApiClient'
 import SupportStrategyService from './supportStrategyService'
 import aValidSupportStrategyDto from '../testsupport/supportStrategyDtoTestDataBuilder'
 import { aValidCreateSupportStrategiesRequest } from '../testsupport/supportStrategyRequestTestDataBuilder'
-import { aValidSupportStrategyListResponse } from '../testsupport/supportStrategyResponseTestDataBuilder'
-import { toSupportStrategyResponseDtos } from '../data/mappers/supportStrategyResponseDtoMapper'
+import {
+  aValidSupportStrategyListResponse,
+  aValidSupportStrategyResponse,
+} from '../testsupport/supportStrategyResponseTestDataBuilder'
+import aValidSupportStrategyResponseDto from '../testsupport/supportStrategyResponseDtoTestDataBuilder'
+import SupportStrategyType from '../enums/supportStrategyType'
+import SupportStrategyCategory from '../enums/supportStrategyCategory'
 
 jest.mock('../data/supportAdditionalNeedsApiClient')
 
@@ -62,18 +69,78 @@ describe('supportStrategyService', () => {
   describe('getSupportStrategies', () => {
     it('should get support strategies', async () => {
       // Given
-      supportAdditionalNeedsApiClient.createEducationSupportPlan.mockResolvedValue(null)
-      const expectedSupportStrategyListResponse = aValidSupportStrategyListResponse()
+      const supportStrategiesListResponse = aValidSupportStrategyListResponse({
+        supportStrategies: [
+          aValidSupportStrategyResponse({
+            active: true,
+            detail: 'Using flash cards with John can help him retain facts',
+            supportStrategyType: 'MEMORY',
+            supportStrategyCategory: 'MEMORY',
+            reference: 'c88a6c48-97e2-4c04-93b5-98619966447b',
+            createdBy: 'asmith_gen',
+            createdByDisplayName: 'Alex Smith',
+            createdAt: '2023-06-19T09:39:44Z',
+            createdAtPrison: 'MDI',
+            updatedBy: 'asmith_gen',
+            updatedByDisplayName: 'Alex Smith',
+            updatedAt: '2023-06-19T09:39:44Z',
+            updatedAtPrison: 'MDI',
+          }),
+        ],
+      })
+      supportAdditionalNeedsApiClient.getSupportStrategies.mockResolvedValue(supportStrategiesListResponse)
 
-      supportAdditionalNeedsApiClient.getSupportStrategies.mockResolvedValue(expectedSupportStrategyListResponse)
+      const expectedSupportStrategies = [
+        aValidSupportStrategyResponseDto({
+          active: true,
+          details: 'Using flash cards with John can help him retain facts',
+          supportStrategyCategoryTypeCode: SupportStrategyType.MEMORY,
+          supportStrategyCategory: SupportStrategyCategory.MEMORY,
+          reference: 'c88a6c48-97e2-4c04-93b5-98619966447b',
+          createdBy: 'asmith_gen',
+          createdByDisplayName: 'Alex Smith',
+          createdAt: parseISO('2023-06-19T09:39:44Z'),
+          createdAtPrison: 'MDI',
+          updatedBy: 'asmith_gen',
+          updatedByDisplayName: 'Alex Smith',
+          updatedAt: parseISO('2023-06-19T09:39:44Z'),
+          updatedAtPrison: 'MDI',
+        }),
+      ]
 
       // When
-      const result = await service.getSupportStrategies(username, 'A1234BC')
+      const actual = await service.getSupportStrategies(username, prisonNumber)
 
       // Then
-      expect(supportAdditionalNeedsApiClient.getSupportStrategies).toHaveBeenCalledWith('A1234BC', username)
-      expect(result).toHaveLength(1)
-      expect(result).toEqual(toSupportStrategyResponseDtos(expectedSupportStrategyListResponse))
+      expect(actual).toEqual(expectedSupportStrategies)
+      expect(supportAdditionalNeedsApiClient.getSupportStrategies).toHaveBeenCalledWith(prisonNumber, username)
+    })
+
+    it('should return empty array of Support Strategies given API returns null', async () => {
+      // Given
+      supportAdditionalNeedsApiClient.getSupportStrategies.mockResolvedValue(null)
+
+      const expectedSupportStrategies = [] as Array<SupportStrategyResponseDto>
+
+      // When
+      const actual = await service.getSupportStrategies(username, prisonNumber)
+
+      // Then
+      expect(actual).toEqual(expectedSupportStrategies)
+      expect(supportAdditionalNeedsApiClient.getSupportStrategies).toHaveBeenCalledWith(prisonNumber, username)
+    })
+
+    it('should rethrow error given API client throws error', async () => {
+      // Given
+      const expectedError = new Error('Internal Server Error')
+      supportAdditionalNeedsApiClient.getSupportStrategies.mockRejectedValue(expectedError)
+
+      // When
+      const actual = await service.getSupportStrategies(username, prisonNumber).catch(e => e)
+
+      // Then
+      expect(actual).toEqual(expectedError)
+      expect(supportAdditionalNeedsApiClient.getSupportStrategies).toHaveBeenCalledWith(prisonNumber, username)
     })
   })
 })
