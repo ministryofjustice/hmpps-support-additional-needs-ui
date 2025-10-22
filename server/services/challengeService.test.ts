40c1794f19d5f763596e4865e34feeaf3e7af196
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2025-10-22 15:55:55 +0100

    RR-1856 - Improve Challenge mapper and tests (#408)

--- server/services/challengeService.test.ts
+++ server/services/challengeService.test.ts
@@ -1,9 +1,10 @@
+import type { ChallengeResponseDto } from 'dto'
 import SupportAdditionalNeedsApiClient from '../data/supportAdditionalNeedsApiClient'
 import ChallengeService from './challengeService'
 import aValidChallengeDto from '../testsupport/challengeDtoTestDataBuilder'
 import { aValidCreateChallengesRequest } from '../testsupport/challengeRequestTestDataBuilder'
 import { aValidChallengeListResponse, aValidChallengeResponse } from '../testsupport/challengeResponseTestDataBuilder'
-import { toChallengeDto } from '../data/mappers/challengeDtoMapper'
+import aValidChallengeResponseDto from '../testsupport/challengeResponseDtoTestDataBuilder'
 
 jest.mock('../data/supportAdditionalNeedsApiClient')
 
@@ -59,23 +60,64 @@ describe('challengeService', () => {
       )
     })
   })
+
   describe('getChallenges', () => {
     it('should get challenges', async () => {
       // Given
-      supportAdditionalNeedsApiClient.createEducationSupportPlan.mockResolvedValue(null)
-      aValidChallengeResponse()
-      aValidChallengeDto()
-      const expectedChallengeListResponse = aValidChallengeListResponse()
+      const challengeListResponse = aValidChallengeListResponse({
+        challengeResponses: [
+          aValidChallengeResponse({
+            alnScreenerDate: null,
+            fromALNScreener: false,
+            symptoms: 'John struggles to read text on white background',
+            howIdentifiedOther: 'John was seen to have other challenges',
+          }),
+        ],
+      })
+      supportAdditionalNeedsApiClient.getChallenges.mockResolvedValue(challengeListResponse)
 
-      supportAdditionalNeedsApiClient.getChallenges.mockResolvedValue(expectedChallengeListResponse)
+      const expectedChallenges = [
+        aValidChallengeResponseDto({
+          alnScreenerDate: null,
+          fromALNScreener: false,
+          symptoms: 'John struggles to read text on white background',
+          howIdentifiedOther: 'John was seen to have other challenges',
+        }),
+      ]
 
       // When
-      const result = await service.getChallenges(username, 'A1234BC')
+      const actual = await service.getChallenges(username, prisonNumber)
 
       // Then
-      expect(supportAdditionalNeedsApiClient.getChallenges).toHaveBeenCalledWith('A1234BC', username)
-      expect(result).toHaveLength(1)
-      expect(result).toEqual(toChallengeDto(expectedChallengeListResponse))
+      expect(actual).toEqual(expectedChallenges)
+      expect(supportAdditionalNeedsApiClient.getChallenges).toHaveBeenCalledWith(prisonNumber, username)
+    })
+
+    it('should return empty Challenges array given API returns null', async () => {
+      // Given
+      supportAdditionalNeedsApiClient.getChallenges.mockResolvedValue(null)
+
+      const expectedChallenges = [] as Array<ChallengeResponseDto>
+
+      // When
+      const actual = await service.getChallenges(username, prisonNumber)
+
+      // Then
+      expect(actual).toEqual(expectedChallenges)
+      expect(supportAdditionalNeedsApiClient.getChallenges).toHaveBeenCalledWith(prisonNumber, username)
+    })
+
+    it('should rethrow error given API client throws error', async () => {
+      // Given
+      const expectedError = new Error('Internal Server Error')
+      supportAdditionalNeedsApiClient.getChallenges.mockRejectedValue(expectedError)
+
+      // When
+      const actual = await service.getChallenges(username, prisonNumber).catch(e => e)
+
+      // Then
+      expect(actual).toEqual(expectedError)
+      expect(supportAdditionalNeedsApiClient.getChallenges).toHaveBeenCalledWith(prisonNumber, username)
     })
   })
 })
