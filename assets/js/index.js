ab3dd0455a777e606a9ff249a02781be1f69eb29
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2025-10-23 11:43:37 +0100

    RR-1970 - Prevent anchor links being double clicked

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,12 +3,14 @@ import * as mojFrontend from '@ministryofjustice/frontend'
 import initBackLinks from './backLink.mjs'
 import initPrintLinks from './print.mjs'
 import applicationInsights from './applicationinsights.mjs'
+import preventAnchorLinkDoubleClicks from './preventAnchorLinkDoubleClicks.mjs'
 
 govukFrontend.initAll()
 mojFrontend.initAll()
 
 initBackLinks()
 initPrintLinks()
+preventAnchorLinkDoubleClicks()
 
 window.initApplicationInsights = (connectionString, applicationInsightsRoleName, authenticatedUser) => {
   applicationInsights.init(connectionString, applicationInsightsRoleName, authenticatedUser)
